c542ceb63ae92eeed74aba121e849bfb2bcbc619
smahaduzzaman <smahaduzzaman96@gmail.com>
2022-10-26 20:19:09 +0600

    Fourth Commt for Update Context Api

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Container from 'react-bootstrap/Container';
 import { Link } from 'react-router-dom';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Sidebar from '../Sidebar/Sidebar';
+import { AuthContext } from '../../../contexts/AuthProvider/AuthProvider';
 
 const Header = () => {
+    const { user } = useContext(AuthContext)
     return (
         <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
             <Container>
@@ -19,7 +21,7 @@ const Header = () => {
                         <Link className='mx-3' to="/category">Category</Link>
                     </Nav>
                     <Nav>
-                        <Nav.Link to="/login">Login</Nav.Link>
+                        <Nav.Link to="/login">{user?.displayName}</Nav.Link>
                         <Nav.Link eventKey={2} to="/register">
                             Register
                         </Nav.Link>
---
+++ src/contexts/AuthProvider/AuthProvider.js
@@ -0,0 +1,18 @@
+import React, { createContext } from 'react';
+
+export const AuthContext = createContext()
+
+const AuthProvider = ({ children }) => {
+    const user = { displayName: 'Arisha Protichi' }
+
+
+    const authInfo = { user }
+    return (
+
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default AuthProvider;
